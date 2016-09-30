62d5c855ab0d482b4a1d9894c98df14245f42303
Michael McLellan <jmikem825@gmail.com>
2016-09-30 12:52:00 -0400

    Use new domain gnip-stream.twitter.com

--- core/src/main/java/com/zaubersoftware/gnip4j/api/impl/PowerTrackV2UriStrategy.java
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/impl/PowerTrackV2UriStrategy.java
@@ -25,7 +25,7 @@ import com.zaubersoftware.gnip4j.api.UriStrategy;
  *
  * As of 10/1/2015 PowerTrack V2 is in private beta.
  *
- * https://stream-data-api.twitter.com/stream/powertrack/accounts/<account>/publishers/twitter/<stream>.json
+ * https://gnip-stream.twitter.com/stream/powertrack/accounts/<account>/publishers/twitter/<stream>.json
  * https://gnip-api.twitter.com/rules/powertrack/accounts/<account>/publishers/twitter/<stream>.json"
  *
  * <p>
@@ -40,7 +40,7 @@ import com.zaubersoftware.gnip4j.api.UriStrategy;
  * @since 10/01/15
  */
 public final class PowerTrackV2UriStrategy implements UriStrategy {
-    public static final String DEFAULT_STREAM_URL_BASE  = "https://stream-data-api.twitter.com";
+    public static final String DEFAULT_STREAM_URL_BASE  = "https://gnip-stream.twitter.com";
     public static final String DEFAULT_RULE_URL_BASE = "https://gnip-api.twitter.com";
     public static final String PATH_GNIP_STREAM_URI =  "/stream/powertrack/accounts/%s/publishers/%s/%s.json";
     public static final String PATH_GNIP_RULES_URI =  "/rules/powertrack/accounts/%s/publishers/%s/%s.json";
